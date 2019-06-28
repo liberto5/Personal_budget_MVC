89654daefecd8c1c614a5c728f355019b93e734d
AdamGolub <adam.golub.programista@gmail.com>
2019-06-28 16:59:13 +0200

    Added username remebering funcionality

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\User;
 use \App\Auth;
+use \App\Flash;
 
 /**
  * Login controller
@@ -22,17 +23,22 @@ class Login extends \Core\Controller
     public function createAction()
     {
         $user = User::authenticate($_POST['email'], $_POST['password']);
+		
+		$remember_me = isset($_POST['remember_me']);
 
         if ($user) {
 			
-            Auth::login($user);
+            Auth::login($user, $remember_me);
 
             $this->redirect(Auth::getReturnToPage());
 
         } else {
+			
+            Flash::addMessage('Incorrect e-mail or password!');
 
             View::renderTemplate('Home/index.html', [
-                'email' => $_POST['email']
+                'email' => $_POST['email'],
+				'remember_me' => $remember_me
             ]);
         }
     }
@@ -41,6 +47,20 @@ class Login extends \Core\Controller
     {
         Auth::logout();
 
-        $this->redirect('/');       
+        $this->redirect('/login/show-logout-message');     
+    }
+	
+	/**
+     * Show a "logged out" flash message and redirect to the homepage. Necessary to use the flash messages
+     * as they use the session and at the end of the logout method (destroyAction) the session is destroyed
+     * so a new action needs to be called in order to use the session.
+     *
+     * @return void
+     */
+    public function showLogoutMessageAction()
+    {
+      Flash::addMessage('Logout successful');
+
+      $this->redirect('/');
     }
 }
